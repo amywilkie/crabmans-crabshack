6b8723d6e3db72362b417de028f81984bde92303
Chris Game and Peji Faghihi <peji.faghihi@gmail.com>
2015-01-21 11:49:00 +0000

    removes memcache and uses ordinary javascript object for store

---
+++ server/fixtures/menu.js
@@ -0,0 +1,52 @@
+module.exports = {
+  'Prawn Cocktail': {
+    type: 'starter',
+    description: 'A modern starter for the modern man',
+    price: '3.99'
+  },
+  'Crab Platter': {
+    type: 'starter',
+    description: 'Truly crabby platter',
+    price: '8.00'
+  },
+  'Claw of Crabulon': {
+    type: 'main',
+    description: 'Keep calm and Crabulon',
+    price: '12.85'
+  },
+  'Crab and Chips': {
+    type: 'main',
+    description: 'A crabby take on a classic',
+    price: '10.49'
+  },
+  'Vegetarian Crab': {
+    type: 'main',
+    description: 'Crab meat made from the best nautical tofu',
+    price: '11.99'
+  },
+  'Crabman Sundae': {
+    type: 'dessert',
+    description: 'Ground up crabs with animal fat ice cream. A variety of sauces available.',
+    price: '4.90'
+  },
+  'Craw Brulee': {
+    type: 'dessert',
+    description: 'Brulee made with the finest claw in Brentford',
+    price: '3.80'
+  },
+  'Crabwerries & Seawater': {
+    type: 'dessert',
+    description: 'Organically grown crawberries with distilled sea water imported from the Pacific',
+    price: '4.99'
+  },
+  'Crabba Cola': {
+    type: 'beverage',
+    description: 'Coca-cola but with our unique distinct crabby flavour',
+    price: '1.80'
+  },
+  'Alcoholic milkshake': {
+    type: 'beverage',
+    description: 'The best alcoholic milkshake you will taste this side of the Atlantic.',
+    price: '3.99'
+  }
+};
